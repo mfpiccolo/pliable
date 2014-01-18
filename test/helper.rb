2df53e277e19ab458fa9c295834d5b8c4e2e7a1d
Mike Piccolo <mfpiccolo@gmail.com>
2014-01-17 21:56:24 -0800

    Add version test

---
+++ test/helper.rb
@@ -0,0 +1,23 @@
+include Pliable
+
+begin
+  require "bundler"
+rescue LoadError => e
+  STDERR.puts e.message
+  STDERR.puts "Run `gem install bundler` to install Bundler."
+  exit e.status_code
+end
+
+begin
+  Bundler.setup(:default, :development, :test)
+rescue Bundler::BundlerError => e
+  STDERR.puts e.message
+  STDERR.puts "Run `bundle install` to install missing gems."
+  exit e.status_code
+end
+
+require "minitest/autorun"
+require "pliable"
+require "mocha/setup"
+require "pry"
+require "pry-debugger"
