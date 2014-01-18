2fa67a46763fbb0f76bb4b50691d65a52f857b25
Mike Piccolo <mfpiccolo@gmail.com>
2014-01-18 01:19:37 -0800

    Add ply relation to plaible

--- lib/pliable.rb
+++ lib/pliable.rb
@@ -1,9 +1,11 @@
 require "pliable/version"
 require "pliable/ply"
+require "pliable/ply_relation"
 
 module Pliable
   def self.included(base)
     base.send(:include, Pliable::Ply)
+    base.send(:include, Pliable::PlyRelation)
     super
   end
 end
